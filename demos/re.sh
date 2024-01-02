7187d6e2fb41ac643be8d33ee37f4e4190a27c4a
Furkathertaha <zzw483@sjtu.edu.cn>
2024-01-02 16:50:05 +0800

    generate demos

---
+++ demos/re.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set the directory path
+directory="/home/jim/TEST/OpenGL_Final_PROJ/new/GL-Rendering/demos"
+
+# Set the common prefix to be removed
+prefix="xxx "
+
+# Change to the directory
+cd "$directory" || exit
+
+# Rename files in the directory
+for file in "$prefix"*; do
+    newname="${file#$prefix}"
+    mv "$file" "$newname"
+done
+
