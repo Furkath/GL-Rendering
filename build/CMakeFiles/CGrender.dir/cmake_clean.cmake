ada68e4ac22433f643a395027f75e1a5f70f64ab
Furkathertaha <zzw483@sjtu.edu.cn>
2024-01-02 15:36:19 +0800

    provide cmake file

---
+++ build/CMakeFiles/CGrender.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CGrender"
+  "CGrender.pdb"
+  "CMakeFiles/CGrender.dir/project.cpp.o"
+  "CMakeFiles/CGrender.dir/project.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CGrender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/CGproject.dir/DependInfo.cmake"
+  "CMakeFiles/CGrender.dir/DependInfo.cmake"
   )
